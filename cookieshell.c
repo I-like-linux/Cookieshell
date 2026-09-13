817db5444c74e262dd7732c40bfab741f3e64b57
I-like-linux <tux.linux.sep.17.1991@gmail.com>
2026-09-13 12:36:40 -0400

    Update cookieshell.c

--- cookieshell.c
+++ cookieshell.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main() {
 
@@ -11,11 +14,14 @@ int main() {
     while (running) {
         char MainInput[256]; // allocates 256 bytes to MainInput 
         char Pointer[] = ">"; // pointer, deafault is ">"
-        char PreCursor[] = "main"; // the array of chars printed before the pointer, default is "main"
+        char PreCursor[1024]; // current file path
         char User[] = "user"; // the user
         char UserSeperator = '@'; // the symbol between the username and the PreCursor when printed in the main promt, default is "@"
 
-        printf("\e[38;2;130;200;200m%s\e[38;2;150;255;150m%c\e[38;2;0;150;150m%s\e[0m%s", User, UserSeperator, PreCursor, Pointer); // 130;200;200 . 150;255;255 . 0;150;150 . 255;255;255
+	// detect the current path and use it as the PreCursor
+	getcwd(PreCursor, sizeof(PreCursor));
+
+        printf("\e[38;2;130;200;200m%s\e[38;2;150;255;150m%c\e[38;2;0;150;150m%s\e[0m%s ", User, UserSeperator, PreCursor, Pointer); // 130;200;200 . 150;255;255 . 0;150;150 . 255;255;255
 	fgets(MainInput, sizeof(MainInput), stdin);
         
         if (strcmp(MainInput, "exit\n") == 0) {
@@ -24,7 +30,53 @@ int main() {
         if (strcmp(MainInput, "clear\n") == 0) {
             printf("\e[2J\e[H");
         }
+
+	if (strcmp(MainInput, "list\n") == 0) {
+            DIR *d = opendir(".");
+            struct dirent *entry = readdir(d);
+	    while ((entry = readdir(d)) != NULL) {
+	        printf("%s | ", entry->d_name);
+	    }
+	    closedir(d);
+	    printf("\n");
+	}
+        if (strcmp(MainInput, "alist\n") == 0) {
+            DIR *d = opendir(".");
+            struct dirent *entry = readdir(d);
+	    while ((entry = readdir(d)) != NULL) {
+	        printf("%s\n", entry->d_name);
+	    }
+	}
+
+	if (strcmp(MainInput, "chdir\n") == 0) {
+            char ChdirInput[1024];
+	    printf("%s ", Pointer);
+	    fgets(ChdirInput, sizeof(ChdirInput), stdin);
+	    ChdirInput[strcspn(ChdirInput, "\n")] = 0;
+	    chdir(ChdirInput);
+	}
+
+	if (strcmp(MainInput, "del\n") == 0) {
+	    char DelInput[1024];
+	    printf("%s ", Pointer);
+	    fgets(DelInput, sizeof(DelInput), stdin);
+	    DelInput[strcspn(DelInput, "\n")] = 0;
+	    remove(DelInput);
+	}
+	if (strcmp(MainInput, "deldir\n") == 0) {
+            char DeldirInput[1024];
+	    printf("%s ", Pointer);
+	    fgets(DeldirInput, sizeof(DeldirInput), stdin);
+	    DeldirInput[strcspn(DeldirInput, "\n")] = 0;
+	    rmdir(DeldirInput);
+	}
+	if (strcmp(MainInput, "mkfle\n") == 0) {
+            char MkfleInput[1024];
+	    printf("%s ", Pointer);
+	    fgets(MkfleInput, sizeof(MkfleInput), stdin);
+	    MkfleInput[strcspn(MkfleInput, "\n")] = 0;
+	    int fd = open(MkfleInput, O_CREAT | O_EXCL | O_WRONLY, 0644);
+	}
     }
     return 0;
-} 
-
+}
