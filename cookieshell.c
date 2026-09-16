f9c3c4eacaf9192ecdec2129ed3104cfaaeb7719
I-like-linux <tux.linux.sep.17.1991@gmail.com>
2026-09-16 08:05:43 -0400

    Update cookieshell.c

--- cookieshell.c
+++ cookieshell.c
@@ -4,6 +4,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 int main() {
 
@@ -12,8 +13,9 @@ int main() {
     bool running = true;
 
     while (running) {
-        char MainInput[256]; // allocates 256 bytes to MainInput 
+        char MainInput[1024]; // allocates 256 bytes to MainInput 
         char Pointer[] = ">"; // pointer, deafault is ">"
+        char ArgPointer[] = "↳"; // pointer for arguments in 
         char PreCursor[1024]; // current file path
         char User[] = "user"; // the user
         char UserSeperator = '@'; // the symbol between the username and the PreCursor when printed in the main promt, default is "@"
@@ -50,7 +52,7 @@ int main() {
 
 	if (strcmp(MainInput, "chdir\n") == 0) {
             char ChdirInput[1024];
-	    printf("%s ", Pointer);
+	    printf(" %s ", ArgPointer);
 	    fgets(ChdirInput, sizeof(ChdirInput), stdin);
 	    ChdirInput[strcspn(ChdirInput, "\n")] = 0;
 	    chdir(ChdirInput);
@@ -58,24 +60,48 @@ int main() {
 
 	if (strcmp(MainInput, "del\n") == 0) {
 	    char DelInput[1024];
-	    printf("%s ", Pointer);
+	    printf(" %s ", ArgPointer);
 	    fgets(DelInput, sizeof(DelInput), stdin);
 	    DelInput[strcspn(DelInput, "\n")] = 0;
 	    remove(DelInput);
 	}
 	if (strcmp(MainInput, "deldir\n") == 0) {
             char DeldirInput[1024];
-	    printf("%s ", Pointer);
+	    printf(" %s ", ArgPointer);
 	    fgets(DeldirInput, sizeof(DeldirInput), stdin);
 	    DeldirInput[strcspn(DeldirInput, "\n")] = 0;
 	    rmdir(DeldirInput);
 	}
 	if (strcmp(MainInput, "mkfle\n") == 0) {
             char MkfleInput[1024];
-	    printf("%s ", Pointer);
+	    printf(" %s ", ArgPointer);
 	    fgets(MkfleInput, sizeof(MkfleInput), stdin);
 	    MkfleInput[strcspn(MkfleInput, "\n")] = 0;
 	    int fd = open(MkfleInput, O_CREAT | O_EXCL | O_WRONLY, 0644);
+	}
+	if (strcmp(MainInput, "repeat\n") == 0) {
+	    char RepeatInput[2048]; // doubble the usual 1024 so larger entries can fit in repeat
+	    printf(" %s ", ArgPointer);
+            fgets(RepeatInput, sizeof(RepeatInput), stdin);
+	    RepeatInput[strcspn(RepeatInput, "\n")] = 0;
+	    printf("%s\n", RepeatInput);
+	}
+	if (strcmp(MainInput, "exec:\n") == 0) {
+            char ExecPathInput[2048];
+	    printf(" %s ", ArgPointer);
+	    fgets(ExecPathInput, sizeof(ExecPathInput), stdin);
+	    char ExecPath[2048];
+	    ExecPathInput[strcspn(ExecPathInput, "\n")] = 0;
+	    snprintf(ExecPath, sizeof(ExecPath), "/bin/%s", ExecPathInput);
+	    printf("%s%s\n", "attempting to excute file: ", ExecPath);
+	    pid_t pid = fork();
+            if (pid == 0) {
+                execl(ExecPath, ExecPath, NULL);
+		perror("exec failed");
+		_exit(1);
+	    } else {
+                waitpid(pid, NULL, 0);
+	    }
 	}
     }
     return 0;
